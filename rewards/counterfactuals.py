bce32d0a125afc543384a9cea53e44e626ece71e
Alexa <alb146@utulsa.edu>
2018-07-03 09:51:10 -0700

    removed add_agent()

--- rewards/counterfactuals.py
+++ rewards/counterfactuals.py
@@ -1,12 +1,11 @@
 from math import sqrt
 
-# Assumes that given domain state is at a specified time step and specific agent id and location is given
-# HOW WILL THE DOMAIN STATE BE GIVEN?
+# Assumes that given domain state is at a specified time step and agent location is given
 def cf(domain_state, agent_info, coupling = 1, consideration_radius = 10.0):
     """
     Implement counterfactual and return new state of the domain
     :param domain_state: current state of the domain
-    :param agent: agent considering counterfactual agents
+    :param agent_info: agent considering counterfactual agents
     :param coupling: the number of agents that must observe POIs for max reward
     :param consideration_radius: the maximum distance from an agent POIs must be to have a counterfactual agent added
     :returns: domain state with counterfactual implemented
@@ -22,7 +21,6 @@ def cf(domain_state, agent_info, coupling = 1, consideration_radius = 10.0):
         dist = distance(agent_info['loc'], poi_info['loc'])
         # If the distance between the agent and poi is less than the consideration radius, store the POI location
         if dist <= consideration_radius:
-            # NOT SURE IF RIGHT THING
             poi_locations.append(poi_info)
 
     # list of POI locations that with an additional agent increase the reward
@@ -37,15 +35,17 @@ def cf(domain_state, agent_info, coupling = 1, consideration_radius = 10.0):
         return domain_state
     else:
         # for each poi in range of the agent, place an additional agent and check for an increase in reward
-        for poi_loc in poi_locations and len(poi_considered) < coupling - 1:
+        for poi in poi_locations and len(poi_considered) < coupling - 1:
             # Calculate D++ 1
-            cf_diff = cf_d(domain_state, poi_loc)
+            cf_diff = cf_d(domain_state, poi)
             if cf_diff > agent_diff:
-                poi_considered.append(poi_loc)
+                poi_considered.append(poi)
 
-    # place agents at considered pois in new domain
+    # create a new agent at each considered poi
+    # HOW DO??
     for considered_poi in poi_considered:
-        new_domain_state = add_agent(new_domain_state, considered_poi)
+        new_domain_state['agents'].
+
     return new_domain_state
 
 
@@ -61,24 +61,13 @@ def distance(loc_1, loc_2):
     return sqrt(square(loc_1[0] - loc_2[0]) + square(loc_1[1] - loc_2[1]))
 
 
-def cf_d(domain_state, poi_locations):
+def cf_d(domain_state, poi):
     """
     Calculate D++ with additional agents placed at specified POIs
     :param domain_state: domain state to modify
-    :param poi_locations: pois to place additional agents at
-    :returns:
+    :param poi: pois to place additional agents at
+    :returns: D++ evaluation
     """
     d_eval =
 
     return d_eval
-
-
-def add_agent(domain_state, poi_location):
-    """
-    Modify agent domain to contain an additional agent at indicated location
-    :param domain_state: domain state to modify
-    :param poi_location: location to place additional agent
-    :returns: modified domain state
-    """
-
-    return domain_state
