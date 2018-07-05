19af699222484d9654d6ba0be75191cd20b04341
Alexa <alb146@utulsa.edu>
2018-07-05 16:25:48 -0700

    Test class includes function test_config() to run domain simulation with specific configuration file. test_up(), meant to check if action implementation works, still in progress, may not be correct

--- test_class.py
+++ test_class.py
@@ -1,12 +1,15 @@
+from run_simulation import main
 from simulators.rover_domain_simulator import RoverDomain
 
+
 class TestClass(RoverDomain):
+    def test_config(self):
+        main("config.yml")
+
     def test_up(self):
         prev_loc = self.agents[0]['loc']
-        dy = prev_loc[]
-        actions = {'agents':[ , ]}
+        dy = prev_loc[1] + 1
+        actions = {'agents': {'loc': [prev_loc[0], dy]}}
         RoverDomain.apply_actions(self, actions)
-        assert  ['loc'][1] >
-
-
+        assert self.agents[0]['loc'][1] > prev_loc[1]
 
