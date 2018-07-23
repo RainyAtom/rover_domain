5976ccbe15cb0895e38d982a1b44601ddbb22117
Alexa <alb146@utulsa.edu>
2018-07-23 11:42:55 -0700

    Saves poi and agent states and global reward to files

--- run_simulation.py
+++ run_simulation.py
@@ -6,12 +6,18 @@ from policies.policy import Evo_MLP
 from rewards.g import GlobalReward
 import yaml
 import sys
+import time
 
-def EvaluateTeam(team, domain, reward, steps):
+def EvaluateTeam(team, domain, reward, steps, flag):
+    states = []
     for step in range(steps):
-        #print(domain)
         # Get States from Rover Doman
         joint_state = domain.get_jointstate()
+
+        if flag is True:
+            # Store trajectories
+            for i in joint_state['agents']:
+                states.append(str(i) + ', ' + str(joint_state['agents'][i]['loc'][0]) + ', ' + str(joint_state['agents'][i]['loc'][1]) + ', ' + str(joint_state['agents'][i]['theta']))
 
         # Get the actions from the team
         actions = team.get_jointaction(joint_state)
@@ -25,19 +31,20 @@ def EvaluateTeam(team, domain, reward, steps):
 
     # Compute the Global Reward
     reward_G = reward.calculate_reward()
-    # print("Reward: ", reward_G)
-    # print()
-    return team, domain, reward_G
+
+    return team, domain, reward_G, states
 
 def main(config_f):
     """
     """
+    # Unique identifier for each run
+    id = str(time.clock())
+
     # Read and store parameters from configuration file.
     if config_f is None:
         config_f = "config.yml"
     with open(config_f, 'r') as f:
         config = yaml.load(f)
-
     print()
     print(config_f)
 
@@ -62,12 +69,37 @@ def main(config_f):
         config["Observation Radius"],
         config["Minimum Distance"])
 
+    g_reward = []
+    # Boolean flag used to indicate when to save trajectories
+    flag = False
+
     for generation in range(config["Epochs"]):
-        team, domain, fitness = EvaluateTeam(team, domain, global_reward, config["Steps"])
+        if generation > 9989:
+            flag = True
+
+        team, domain, fitness, trajectories = EvaluateTeam(team, domain, global_reward, config["Steps"], flag)
+
+        # Store trajectories in a file
+        with open(id + '_agent_states.yml', 'a') as file:
+            for x in trajectories:
+                file.write(x + "\n")
+
+        g_reward.append(str(fitness['agent_0']))
         print(fitness['agent_0'])
+
         # CCEA Evaluation
         CCEA(team, fitness)
 
+    # Store POI states in file
+    joint_state = domain.get_jointstate()
+    with open(id +'_poi_states.yml', 'w') as file:
+        for i in joint_state['pois']:
+            file.write(str(i) + ',' + str(joint_state['pois'][i]['loc']) + "\n")
+    # Store the global reward in a file
+    with open(id +'_global_reward.yml', 'w') as file:
+        file.write(config_f + "\n")
+        for x in g_reward:
+            file.write(x + "\n")
 
 if __name__ == '__main__':
     # When ran through command line and no specific file is indicated, use default configuration file
